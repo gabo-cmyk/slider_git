2c4aed235f7ad36918f32ee3ccc3394568b533fd
Matyi <matyas.csorba@melkweg.hu>
2020-07-09 17:45:45 +0200

    some modifications

--- js/scrpit.js
+++ js/scrpit.js
@@ -1,24 +1,20 @@
 $(function () {
-
     var slides = $('.slides');
 
-    $('.prev-button').on('click', function () {
-
+    var nextSlide = function(direction) {
         $('.slides').last().animate({
-            left: '-100%'
+            left: direction
         }, 'slow', function () {
             $('.slides').first().before($('.slides').last());
             $('.slides').css('left', '0%');
         });
+    };
+
+    $('.prev-button').on('click', function () {
+        nextSlide('-100%');
     });
 
     $('.next-button').on('click', function () {
-        $('.slides').last().animate({
-            left: '100%'
-        }, 'slow', function () {
-            $('.slides').first().before($('.slides').last());
-            $('.slides').css('left', '0%');
-        });
+        nextSlide('100%');
     });
-
 });
