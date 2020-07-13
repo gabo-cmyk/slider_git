a4a0c9478cf35a584f52bfd8145adccc4a24e013
gabo-cmyk <68051765+gabo-cmyk@users.noreply.github.com>
2020-07-13 15:15:02 +0200

    slider_v2

--- js/scrpit.js
+++ js/scrpit.js
@@ -1,20 +1,33 @@
 $(function () {
-    var slides = $('.slides');
 
-    var nextSlide = function(direction) {
-        $('.slides').last().animate({
+    var index = 1;
+    var total = $('.slides').length;
+
+    var nextSlide = function (direction) {
+        $('.active').css('z-index', '3').animate({
             left: direction
         }, 'slow', function () {
-            $('.slides').first().before($('.slides').last());
-            $('.slides').css('left', '0%');
+            $('.slides').css({
+                'left': '0%',
+                'z-index': '1'
+            });
         });
+    };
+
+    var nextSlide2 = function (direction2) {
+        $(".slides").removeClass("active");
+        $(".slides").eq(index).addClass("active").css('z-index', '2');
+        direction2
     };
 
     $('.prev-button').on('click', function () {
         nextSlide('-100%');
+        nextSlide2(index = (index - 1) % total);
     });
 
     $('.next-button').on('click', function () {
         nextSlide('100%');
+        nextSlide2(index = (index + 1) % total)
     });
+
 });
